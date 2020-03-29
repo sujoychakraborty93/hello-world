8b22d26e8310077981632ca742b9ad63179f2cf4
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-03-28 19:10:13 -0700

    added chanegs in branch2

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
+  res.send("hello world. \nThis is the mesage added in branch2")
+})
+app.get("/id", (req,res) => {
+	res.send("this is id. added in branch2")
+})
 app.listen(4000, () => {
   console.log("server running");
 });
