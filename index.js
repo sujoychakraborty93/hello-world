6c2ca4dbc7b813f2ced20ce58d6f6f666a042c54
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-03-29 08:14:49 -0700

    udpated index.js in master

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world. this is from master");
+});
+app.get("/master", (req, res) => {
+  res.send("i am master branch");
 });
 
 app.listen(4000, () => {
