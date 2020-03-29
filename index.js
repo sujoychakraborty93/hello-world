223c44e44d6299c8e8789d1b8bf333e2b7ae1106
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-03-29 08:23:42 -0700

    udpated file in branch2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world. \nThis is the mesage added in branch2")
-})
-app.get("/id", (req,res) => {
-	res.send("this is id. added in branch2")
-})
+  res.send("hello world. \nThis is the mesage added in branch2");
+});
+app.get("/branch2", (req, res) => {
+  res.send("this is from branch2. added in branch2");
+});
 app.listen(4000, () => {
   console.log("server running");
 });
