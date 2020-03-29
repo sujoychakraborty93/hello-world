3ace5930590475d2d2e8621db90d4bf66c85fefc
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-03-29 08:19:02 -0700

    udpated file in readme-edits branch

--- file_created_on_branch.js
+++ file_created_on_branch.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-app.get("/", (req, res) => {
-	res.send("this file was created in branch readme-edit")
-})
+const express = require("express");
+const app = express();
+app.get("/readme-edits", (req, res) => {
+  res.send("this file was created in branch readme-edit");
+});
 app.listen(4000, () => {
-	console.log("listenong on server 4000")
-})
+  console.log("listenong on server 4000");
+});
