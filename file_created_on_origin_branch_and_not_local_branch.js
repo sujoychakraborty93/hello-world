d892352d631d83c98885668d2a4f5d11b6b2cfae
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-04-04 08:15:28 -0700

    udpated master files which were created in branch

--- file_created_on_origin_branch_and_not_local_branch.js
+++ file_created_on_origin_branch_and_not_local_branch.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.get("/readme-edits2", (req, res) => {
+router.get("/", (req, res) => {
   res.send(
     "this file was created in branch readme-edit. file_created_on_origin_branch_and_not_local_branch.js"
   );
 });
 
-app.listen(4000, () => {
-  console.log("server running");
-});
+// app.listen(4000, () => {
+//   console.log("server running");
+// });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const readme1 = require("./file_created_on_branch.js");
+const readme2 = require("./file_created_on_origin_branch_and_not_local_branch.js");
 
 app.get("/", (req, res) => {
   res.send("hello world. this is from master");
@@ -7,6 +9,8 @@ app.get("/", (req, res) => {
 app.get("/master", (req, res) => {
   res.send("i am master branch");
 });
+app.use("/readme-edits1", readme1);
+app.use("/readme-edits2", readme2);
 
 app.listen(4000, () => {
   console.log("server running");
