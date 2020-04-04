c09062ebd321b08f50b544ca184f77cf6efe66ed
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-04-04 08:38:28 -0700

    added index-readme-branch file and modified other files

--- file_created_on_branch.js
+++ file_created_on_branch.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
-app.get("/readme-edits", (req, res) => {
-  res.send("this file was created in branch readme-edit");
+const router = express.Router();
+
+router.get("/:id?", (req, res) => {
+  console.log("inide");
+  if (req.params.id) {
+    res.send(
+      `this file was created in branch readme-edit ith param ${req.params.id}`
+    );
+  } else {
+    res.send("this file was created in branch readme-edit");
+  }
 });
-app.listen(4000, () => {
-  console.log("listenong on server 4000");
-});
+// app.listen(4000, () => {
+//   console.log("listenong on server 4000");
+// });
+
+module.exports = router;
